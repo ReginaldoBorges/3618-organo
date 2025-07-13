0070d1b801943fe2447992bf21573ccacd4b2f4c
ReginaldoBorges <reginaldo.borges07@gmail.com>
2025-07-13 15:23:01 -0300

    Criados 2 testes novos para validar os métodos obterLivrosPorGenero() e adicionarLivro()

--- src/app/services/livro.service.spec.ts
+++ src/app/services/livro.service.spec.ts
@@ -1,3 +1,5 @@
+import { Livro } from '../componentes/livro/livro';
+import { livros } from '../mock-livros';
 import { LivroService } from './livro.service';
 
 /**
@@ -9,5 +11,34 @@ describe('LivroService', () => {
   it('should be created', () => {
     service = new LivroService(); // Create an instance of LivroService
     expect(service).toBeTruthy();
+  });
+
+  it('should return books by genre informed', () => {
+    service = new LivroService(); // Create an instance of LivroService
+    const livrosPorGenero = service.obterLivrosPorGenero('romance'); // Call the method with a genre
+    const LivrosEsperados = livros.filter(
+      (livro) => livro.genero.id === 'romance'
+    ); // Filter expected books by genre
+
+    expect(livrosPorGenero).toEqual(LivrosEsperados); // Check if the returned books match the expected ones
+  });
+
+  it('should add a new book', () => {
+    service = new LivroService(); // Create an instance of LivroService
+    const novoLivro: Livro = {
+      titulo: 'Novo Livro',
+      autoria: 'Autor Desconhecido',
+      imagem: 'https://example.com/imagem.jpg',
+      genero: { id: 'romance', value: 'Romance' },
+      dataLeitura: '2023-10-01',
+      classificacao: 5,
+    };
+
+    // Add the new book using the service
+    service.adicionarLivro(novoLivro);
+
+    // Verify that the book was added by checking if it exists in the list of books for the specified genre
+    const livrosAtualizados = service.obterLivrosPorGenero('romance');
+    expect(livrosAtualizados).toContain(novoLivro);
   });
 });

d4c0673f2b6edb17a10fe821a8441e81ff6d121f
ReginaldoBorges <reginaldo.borges07@gmail.com>
2025-07-13 16:14:48 -0300

    Criados os testes para verificar a inicilização correta de gêneros e mensagens de error

--- src/app/services/livro.service.spec.ts
+++ src/app/services/livro.service.spec.ts
@@ -1,23 +1,23 @@
-import { Livro } from '../componentes/livro/livro';
+import { GeneroLiterario, Livro } from '../componentes/livro/livro';
 import { livros } from '../mock-livros';
-import { LivroService } from './livro.service';
+import { ErroGeneroLiterario, LivroService } from './livro.service';
 
 /**
- * This file contains unit tests for the LivroService class.
+ * Este arquivo contém os testes unitários para a classe LivroService.
  */
 describe('LivroService', () => {
-  let service: LivroService; // Variable to hold the instance of LivroService
+  let service: LivroService; // Variável para armazenar a instância de LivroService
 
-  // Executed before each test
+  // Executado antes de cada teste
   beforeEach(() => {
-    service = new LivroService(); // Create a new instance of LivroService before each test
+    service = new LivroService(); // Cria uma nova instância de LivroService antes de cada teste
   });
 
-  it('should be created', () => {
+  it('Deve ser criado', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should add a new book', () => {
+  it('Deve adicionar um novo livro', () => {
     const novoLivro: Livro = {
       titulo: 'Novo Livro',
       autoria: 'Autor Desconhecido',
@@ -27,20 +27,73 @@ describe('LivroService', () => {
       classificacao: 5,
     };
 
-    // Add the new book using the service
+    // Adiciona o novo livro usando o serviço
     service.adicionarLivro(novoLivro);
 
-    // Verify that the book was added by checking if it exists in the list of books for the specified genre
+    // Verifica se o livro foi adicionado, verificando se ele existe na lista de livros para o gênero especificado
     const livrosAtualizados = service.obterLivrosPorGenero('romance');
     expect(livrosAtualizados).toContain(novoLivro);
   });
 
-  it('should return books by genre informed', () => {
-    const livrosPorGenero = service.obterLivrosPorGenero('romance'); // Call the method with a genre
+  it('Deve retornar livros pelo gênero informado', () => {
+    const livrosPorGenero = service.obterLivrosPorGenero('romance'); // Chama o método com um gênero
     const LivrosEsperados = livros.filter(
       (livro) => livro.genero.id === 'romance'
-    ); // Filter expected books by genre
+    ); // Filtra os livros esperados pelo gênero
 
-    expect(livrosPorGenero).toEqual(LivrosEsperados); // Check if the returned books match the expected ones
+    expect(livrosPorGenero).toEqual(LivrosEsperados); // Verifica se os livros retornados correspondem aos esperados
+  });
+
+  it('Deve inicializar os gêneros corretamente', () => {
+    // Constante com a lista de gêneros esperados
+    const generos: GeneroLiterario[] = [
+      {
+        id: 'romance',
+        value: 'Romance',
+      },
+      {
+        id: 'misterio',
+        value: 'Mistério',
+      },
+      {
+        id: 'fantasia',
+        value: 'Fantasia',
+      },
+      {
+        id: 'ficcao-cientifica',
+        value: 'Ficção Científica',
+      },
+      {
+        id: 'tecnicos',
+        value: 'Técnicos',
+      },
+    ];
+
+    const generosService = service.generos; // Obtém a lista de gêneros do serviço
+
+    expect(generosService).toEqual(generos); // Verifica se os gêneros do serviço correspondem aos esperados
+  });
+
+  it('Deve gerar erro ao adicionar livro com gênero desconhecido', () => {
+    let error: any;
+    const novoLivro: Livro = {
+      titulo: 'Livro Desconhecido',
+      autoria: 'Autor Desconhecido',
+      imagem: 'https://example.com/imagem.jpg',
+      genero: { id: 'desconhecido', value: 'Desconhecido' }, // Gênero desconhecido
+      dataLeitura: '2023-10-01',
+      classificacao: 5,
+    };
+
+    expect(() => service.adicionarLivro(novoLivro)).toThrow(
+      ErroGeneroLiterario
+    ); // Verifica se o erro é lançado
+
+    try {
+      service.adicionarLivro(novoLivro);
+    } catch (e) {
+      error = e;
+    }
+    expect(error.message).toBe('Gênero literário desconhecido'); // Verifica se o erro é lançado com a mensagem correta
   });
 });

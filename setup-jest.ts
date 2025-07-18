4c59065cdda527ebf840be684d4be17e560010b4
ReginaldoBorges <reginaldo.borges07@gmail.com>
2025-07-18 06:20:02 -0300

    Criado o arquivo setup-jest e chamada adicionada no jest.config. Criada a especificação para validar a definição das propriedades alt e src do cabeçalho.

---
+++ setup-jest.ts
@@ -0,0 +1,4 @@
+// jest.setup.ts
+import 'jest-preset-angular/setup-jest';
+import 'zone.js';
+import 'zone.js/testing';
--- src/app/componentes/cabecalho/cabecalho.component.spec.ts
+++ src/app/componentes/cabecalho/cabecalho.component.spec.ts
@@ -1,17 +1,24 @@
-import { TestBed } from '@angular/core/testing'; // Importa TestBed para configurar o ambiente de testes
+import { ComponentFixture, TestBed } from '@angular/core/testing'; // Importa TestBed para configurar o ambiente de testes
 import { CabecalhoComponent } from './cabecalho.component';
 
 describe('CabecalhoComponent', () => {
   let component: CabecalhoComponent; // Variável para armazenar a instância do componente
+  let fixture: ComponentFixture<CabecalhoComponent>; // Variável para armazenar o fixture do componente
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
       imports: [CabecalhoComponent],
     });
-    component = new CabecalhoComponent(); // Cria uma nova instância do componente antes de cada teste
+    fixture = TestBed.createComponent(CabecalhoComponent); // Cria o fixture do componente
+    component = fixture.componentInstance; // Obtém a instância do componente a partir do fixture
   });
 
   it('Deveria criar o componente', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Deveria definir as propriedades alt e src', () => {
+    expect(component.alt).toBeDefined();
+    expect(component.src).toBeDefined();
   });
 });
